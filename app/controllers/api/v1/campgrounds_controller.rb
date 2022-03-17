03463f1f89abdced3dc7bcdc4d453ed3a8d7c8fb
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-17 17:04:40 -0600

    Feat: Add Campground CRUD functionality

--- app/controllers/api/v1/campgrounds_controller.rb
+++ app/controllers/api/v1/campgrounds_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::CampgroundsController < ApplicationController
-end 
+  def index
+    render json: CampgroundSerializer.new(Campground.all)
+  end
+
+  def show
+    render json: CampgroundSerializer.new(Campground.find(params[:id]))
+  end
+
+  def create
+    campground = Campground.create!(campground_params)
+    campsites = Campsite.find_by(params[:campground_id])
+    campground.campsites << campsites
+    render json: CampgroundSerializer.new(campground), status: 201
+  end
+
+  def update
+    campground = Campground.find(params[:id])
+    campground.update!(campground_params)
+    render json: CampgroundSerializer.new(campground)
+  end
+
+  def destroy
+    campground = Campground.find(params[:id])
+    campground.delete
+  end
+
+  private
+
+  def campground_params
+    params.require(:campground).permit(:name)
+  end
+end
