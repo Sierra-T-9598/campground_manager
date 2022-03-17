c8904374b08ca5c598a749f5558428eec4aea9f5
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-16 19:26:56 -0600

    Add `gem shoulda-matchers`

--- spec/models/campground_spec.rb
+++ spec/models/campground_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Campground, type: :model do
-  describe ''
+  describe 'relationships' do
+    it { should_h}
+  end
+
+  describe 'validations' do
+  end
 end
