badb21b05794fdc570379ef68d6d4b15f8539297
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-18 10:00:31 -0600

    Test: Add endpoints for campground availability and pricing

--- spec/requests/api/v1/campground_request_spec.rb
+++ spec/requests/api/v1/campground_request_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe 'Campground Endpoints' do
   let!(:campground_2) { create(:campground, name: "Arches") }
   let!(:campground_3) { create(:campground, name: "Crater Lake") }
 
-  let!(:site_1) { create(:campsite, campground_id: campground_1.id, name: "1" )}
-  let!(:site_2) { create(:campsite, campground_id: campground_1.id, name: "2" ) }
-  let!(:site_3) { create(:campsite, campground_id: campground_2.id) }
-  let!(:site_4) { create(:campsite, campground_id: campground_2.id) }
-  let!(:site_5) { create(:campsite, campground_id: campground_3.id) }
-  let!(:site_6) { create(:campsite, campground_id: campground_3.id) }
+  let!(:site_1) { create(:campsite, price: 10.0, campground_id: campground_1.id, name: "1" )}
+  let!(:site_2) { create(:campsite, price: 10.0, campground_id: campground_1.id, name: "2" ) }
+  let!(:site_3) { create(:campsite, price: 5.0, campground_id: campground_2.id) }
+  let!(:site_4) { create(:campsite, price: 5.0, campground_id: campground_2.id) }
+  let!(:site_5) { create(:campsite, price: 5.0, campground_id: campground_3.id) }
+  let!(:site_6) { create(:campsite, price: 8.0, campground_id: campground_3.id) }
 
-  let!(:booking_1) { create(:booking, campsite_id: site_1.id) }
-  let!(:booking_2) { create(:booking, campsite_id: site_1.id) }
-  let!(:booking_3) { create(:booking, campsite_id: site_2.id) }
-  let!(:booking_4) { create(:booking, campsite_id: site_2.id) }
-  let!(:booking_5) { create(:booking, campsite_id: site_3.id) }
-  let!(:booking_6) { create(:booking, campsite_id: site_3.id) }
+  let!(:booking_1) { create(:booking, date: '5/6/22', campsite_id: site_1.id) }
+  let!(:booking_2) { create(:booking, date: '5/7/22', campsite_id: site_1.id) }
+  let!(:booking_3) { create(:booking, date: '5/8/22', campsite_id: site_1.id) }
+  let!(:booking_4) { create(:booking, date: '5/6/22', campsite_id: site_2.id) }
+  let!(:booking_5) { create(:booking, date: '5/7/22', campsite_id: site_2.id) }
+  let!(:booking_6) { create(:booking, date: '5/8/22', campsite_id: site_2.id) }
 
   describe 'get /campgrounds' do
     describe 'campgrounds exist in database' do
@@ -39,18 +39,6 @@ RSpec.describe 'Campground Endpoints' do
       end
     end
   end
-
-  #   describe 'there are no campgrounds in database' do
-  #     it 'returns an empty JSON string' do
-  #       get '/api/v1/campgrounds'
-  #       campgrounds = JSON.parse(response.body, symbolize_names: true)
-  #       expect(response.status).to eq(200)
-  #
-  #       expect(campgrounds).to have_key(:data)
-  #       expect(campgrounds[:data]).to eq([])
-  #     end
-  #   end
-  # end
 
   describe 'get /campground/:id' do
     describe 'the campground exists' do
@@ -107,12 +95,12 @@ RSpec.describe 'Campground Endpoints' do
     end
   end
 
-  describe 'patch /campgrounds' do
+  describe 'patch /campgrounds/:id' do
     describe 'request to update a campground with existing id' do
       it 'generates an updated campground response' do
-        campground_id = campground_2.id
-        previous_name = campground_2.name
-        expect(previous_name).to eq('Arches')
+        campground_id = campground_1.id
+        previous_name = campground_1.name
+        expect(previous_name).to eq('Moraine Park')
 
         campground_params = { name: 'Aspen Glen' }
         headers = { 'CONTENT_TYPE' => 'application/json'}
@@ -122,7 +110,7 @@ RSpec.describe 'Campground Endpoints' do
 
         expect(response.status).to eq(200)
         expect(updated_campground.name).to eq('Aspen Glen')
-        expect(updated_campground.booked_dates).to eq([booking_5.date, booking_6.date])
+        expect(updated_campground.booked_dates).to be_an Array
         expect(updated_campground.price_range).to be_a String
       end
     end
@@ -174,6 +162,60 @@ RSpec.describe 'Campground Endpoints' do
           delete "/api/v1/campgrounds/#{id}"
           expect(response.status).to eq(404)
         end
+      end
+    end
+  end
+
+  describe 'get /campgrounds?from={date}&to={date}' do
+    describe 'request to get all campgrounds with availability between given dates' do
+      it 'returns a json list of available campgrounds' do
+        from = '5/6/22'
+        to = '5/8/22'
+        get "/api/v1/campgrounds?from=#{from}&to=#{to}"
+
+        campgrounds = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(200)
+
+        expect(campgrounds).to have_key(:data)
+        expect(campgrounds[:data].count).to eq(2)
+
+        campgrounds[:data].each do |campground|
+          expect(campground[:id]).to be_a String
+          expect(campground[:attributes][:name]).to be_a String
+          expect(campground[:attributes][:booked_dates]).to be_an Array
+          expect(campground[:attributes][:price_range]).to be_a String
+        end
+      end
+    end
+  end
+
+  describe 'request to order campgrounds by price' do
+    describe 'get /campgrounds?order=low_to_high' do
+      it 'returns list of campgrounds ordered by lowest average price first' do
+        get "/api/v1/campgrounds?order=low_to_high"
+
+        campgrounds = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(200)
+
+        expect(campgrounds).to have_key(:data)
+        expect(campgrounds[:data].count).to eq(3)
+        expect(campgrounds[:data].first[:attributes][:name]).to eq(campground_2.name)
+        expect(campgrounds[:data].last[:attributes][:name]).to eq(campground_1.name)
+
+      end
+    end
+
+    describe 'get /campgrounds?order=high_to_low' do
+      it 'returns list of campgrounds ordered by highest average price first' do
+        get "/api/v1/campgrounds?order=high_to_low"
+
+        campgrounds = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(200)
+
+        expect(campgrounds).to have_key(:data)
+        expect(campgrounds[:data].count).to eq(3)
+        expect(campgrounds[:data].first[:attributes][:name]).to eq(campground_1.name)
+        expect(campgrounds[:data].last[:attributes][:name]).to eq(campground_2.name)
       end
     end
   end
